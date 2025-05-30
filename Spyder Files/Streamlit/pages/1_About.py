428947bcb51528d3689351acd8348e1497c6ca70
Tim Le <158625868+timphamle@users.noreply.github.com>
2025-05-30 12:03:50 -0400

    fixed the pages

--- Spyder Files/Streamlit/pages/1_About.py
+++ Spyder Files/Streamlit/pages/1_About.py
@@ -2,19 +2,11 @@
 """
 Created on Fri May 30 11:23:25 2025
 
-<<<<<<< HEAD
 @author: heyti
 """
 
 import streamlit as st
 
 st.write("### Question Wording")
-=======
-import streamlit as st
 
-st.title("Page 1")
-st.write("This is the first page.")
-<<<<<<< HEAD:Spyder Files/Streamlit/pages/1_question.py
->>>>>>> da93d96b2768b658158b6d18bfab028c214d1d11
-=======
->>>>>>> b2245149ca3f3d84e4862fef9fe6ddc441b7f453:Spyder Files/Streamlit/pages/1_About.py
+
--- Spyder Files/Streamlit/pages/2_Public_Opinion_Explorer.py
+++ Spyder Files/Streamlit/pages/2_Public_Opinion_Explorer.py
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 30 11:23:25 2025
-=======
-
-import streamlit as st
-
-st.title("Page 2")
-st.write("This is the second page.")
-
-
->>>>>>> da93d96b2768b658158b6d18bfab028c214d1d11
 
 @author: heyti
 """
