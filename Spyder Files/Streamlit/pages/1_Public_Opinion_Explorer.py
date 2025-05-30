d6b5b6e3087b548e0a45a8b7907fb3a49d7e8b91
Tim Le <158625868+timphamle@users.noreply.github.com>
2025-05-30 13:13:08 -0400

    added inputs on public opinion explorer.py

--- Spyder Files/Streamlit/pages/1_Public_Opinion_Explorer.py
+++ Spyder Files/Streamlit/pages/1_Public_Opinion_Explorer.py
@@ -8,3 +8,36 @@ Created on Fri May 30 11:23:25 2025
 import streamlit as st
 
 st.write("### Public Opinion Explorer")
+
+st.markdown("--description of page--")
+
+
+# inputs
+
+col1, col2, col3 = st.columns(3)
+
+study = col1.selectbox(
+    "Study",
+    ("Cooperative Election Study", "American National Election Studies"),
+)
+
+topic = col2.selectbox(
+    "Topic",
+    ("Abortion", "Gun Control"),
+)
+
+group_by = col3.multiselect(
+    "Group By:",
+    ["Political Party", 
+     "Education", 
+     "Employment Status", 
+     "Marriage", 
+     "Income", 
+     "Religion", 
+     "Gender",
+     "Race",
+     "State",
+     "Urban/Rural Status"
+     ],
+    default=["Political Party"],
+)
