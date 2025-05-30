d54a26af331610bc6c5393c57e1704216d63027c
Tim Le <158625868+timphamle@users.noreply.github.com>
2025-05-30 11:59:12 -0400

    added titles

--- Spyder Files/Streamlit/main.py
+++ Spyder Files/Streamlit/main.py
@@ -5,3 +5,6 @@ Created on Fri May 30 11:23:25 2025
 @author: heyti
 """
 
+import streamlit as st
+
+st.title("Home")
--- Spyder Files/Streamlit/pages/2_about.py
+++ Spyder Files/Streamlit/pages/2_about.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Fri May 30 11:23:25 2025
 
+@author: heyti
+"""
+
+import streamlit as st
+
+st.write("### About Page")
