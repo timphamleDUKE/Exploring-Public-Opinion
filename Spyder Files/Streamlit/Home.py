64d7c3979dbb946baef243e26d2ac782a3101e2b
Alexa Fahrer <ajf62@duke.edu>
2025-05-30 11:55:55 -0400

    Started streamlit

---
+++ Spyder Files/Streamlit/Home.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 30 11:23:25 2025
+
+@author: heyti
+"""
+
+import streamlit as st
+
+st.set_page_config(page_title = "Home")
+
+st.title("Home")
+st.write("This is the first page.")
