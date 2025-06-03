978ffd344d029f0feb4e8556eea19e1d71fa2444
Alexa Fahrer <ajf62@duke.edu>
2025-06-03 10:55:33 -0400

    deleted unnecessary files

--- Spyder Files/Streamlit/Home.py
+++ Spyder Files/Streamlit/Home.py
@@ -7,5 +7,10 @@ Created on Fri May 30 11:23:25 2025
 
 import streamlit as st
 
+st.set_page_config(
+    page_title="Survey Navigator",
+    layout="wide"
+)
+
 st.title("The Survey Navigator")
 st.divider()
