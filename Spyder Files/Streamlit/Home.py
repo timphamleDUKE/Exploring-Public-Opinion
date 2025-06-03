90467f0a8fb6301c664ffb7de1a73531f89a5a4f
Alexa Fahrer <ajf62@duke.edu>
2025-06-03 11:58:55 -0400

    added logos

--- Spyder Files/Streamlit/Home.py
+++ Spyder Files/Streamlit/Home.py
@@ -12,5 +12,26 @@ st.set_page_config(
     layout="wide"
 )
 
+LOGO_WIDE = "images/logo.png"
+LOGO_ICON = "images/logo.png"
+
+# Display the logo
+st.logo(
+    image=LOGO_WIDE,
+    size="large",
+    link="https://your-company-website.com", 
+    icon_image=LOGO_ICON
+)
+
+LOGO_PATH = "images/logo.png"
+
+# Display a large logo at the top of the page
+st.image(LOGO_PATH, width=300)
+
+# Add a large logo to the sidebar
+with st.sidebar:
+    st.image(LOGO_PATH, width=300)
+
+
 st.title("The Survey Navigator")
 st.divider()
