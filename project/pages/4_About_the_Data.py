5f5eb7876792f0b6ef0a9ff30173439fa7145463
Alexa Fahrer <ajf62@duke.edu>
2025-06-24 14:15:41 -0400

    added data frame

--- project/pages/4_About_the_Data.py
+++ project/pages/4_About_the_Data.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import base64
 import os
-from functions.dictionaries import set_logo
+from functions.dictionaries import set_logo, df
 
 set_logo()
 st.title("About the Data")
 st.divider()
+
+st.write(df)
