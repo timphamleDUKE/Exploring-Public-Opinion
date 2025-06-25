f5f406fa54f1d216743ee925fcbff9d841626475
Alexa Fahrer <ajf62@duke.edu>
2025-06-25 12:38:21 -0400

    moved sidebar questions

--- project/pages/2_Issue_Position.py
+++ project/pages/2_Issue_Position.py
@@ -4,25 +4,34 @@ import holoviews as hv
 from functions.dictionaries import *
 from functions.sankey import sankeyGraph, display_sankey_streamlit_bokeh
 
-
 set_logo()
-
 st.title("Issue Position")
 
-with st.sidebar:
-    st.title("Customize:")
+col1, col2 = st.columns([1, 2])
 
-    topic = st.selectbox("Topic", list_of_issue_topics, index = 0)
-    list_of_issues = topic_to_list_of_issue_map.get(topic)
-    issue_question = st.selectbox("Issue Question", list_of_issues, index = 0)
-    issue_question = description_to_renamed.get(issue_question)
+with col1:
+    topic = st.selectbox("Topic", list_of_issue_topics, index=0)
 
-    lib_con_pt = st.radio("Groups", ("Lib/Con 2-Point Scale", "Lib/Con 7-Point Scale", "Political Party"))
+list_of_issues = topic_to_list_of_issue_map.get(topic)
+
+with col2:
+    dropdown_issue_question = st.selectbox("Issue Question", list_of_issues, index=0)
+
+issue_question = description_to_renamed.get(dropdown_issue_question)
+lib_con_pt = st.radio("Groups", ("Lib/Con 2-Point Scale", "Lib/Con 7-Point Scale", "Political Party"))
+
+#with st.sidebar:
+#    st.title("Customize:")
+#
+#    topic = st.selectbox("Topic", list_of_issue_topics, index = 0)
+#    list_of_issues = topic_to_list_of_issue_map.get(topic)
+#    issue_question = st.selectbox("Issue Question", list_of_issues, index = 0)
+#    issue_question = description_to_renamed.get(issue_question)
+#    lib_con_pt = st.radio("Groups", ("Lib/Con 2-Point Scale", "Lib/Con 7-Point Scale", "Political Party"))
 
 sankey_graph = (sankeyGraph(df, issue_question, lib_con_pt))
 
 # Display plots
-
 st.markdown(f"### {description_map.get(issue_question)}")
 
 try:
@@ -35,11 +44,8 @@ except:
         display_sankey_streamlit_bokeh(df, issue_question, lib_con_pt)
 
 
-
-
 # Expander
 expander = st.expander("Details")
-
 full_question = full_description_map.get(issue_question)
 
 if pd.notna(full_question):
