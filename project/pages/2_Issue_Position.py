049e9c21005ed41fc4e69fb7ee6bdfdc22c13522
Tim Le <158625868+timphamle@users.noreply.github.com>
2025-06-25 14:20:05 -0400

    big filter changes to sankey graph

--- project/pages/2_Issue_Position.py
+++ project/pages/2_Issue_Position.py
@@ -3,6 +3,7 @@ import pandas as pd
 import holoviews as hv
 from functions.dictionaries import set_logo, list_of_issue_topics, topic_to_list_of_issue_map, description_to_renamed, df, description_map, full_description_map
 from functions.sankey import sankeyGraph, display_sankey_streamlit_bokeh
+from functions.sidebar_sankey import political_check, ideological_check, list_of_groups_check
 
 
 set_logo()
@@ -17,9 +18,21 @@ with st.sidebar:
     issue_question = st.selectbox("Issue Question", list_of_issues, index = 0)
     issue_question = description_to_renamed.get(issue_question)
 
-    lib_con_pt = st.radio("Groups", ("Lib/Con 2-Point Scale", "Lib/Con 7-Point Scale", "Political Party"))
+    group = st.radio("Groups", ["Ideological Groups", "Political Groups"])
 
-sankey_graph = (sankeyGraph(df, issue_question, lib_con_pt))
+    st.markdown(
+        '<div style="font-size: 0.875rem; font-weight: 400; margin-bottom: 0.5rem;">Options</div>',
+        unsafe_allow_html=True
+    )
+
+    if group == "Ideological Groups":
+        checks = ideological_check()
+    else:
+        checks = political_check()
+
+    list_of_groups = list_of_groups_check(group, checks)
+
+sankey_graph = (sankeyGraph(df, issue_question, list_of_groups, group))
 
 # Display plots
 
@@ -32,7 +45,7 @@ except:
         st.plotly_chart(sankey_graph, use_container_width=True)
     except:
         bokeh_plot = hv.render(sankey_graph)
-        display_sankey_streamlit_bokeh(df, issue_question, lib_con_pt)
+        display_sankey_streamlit_bokeh(df, issue_question, list_of_groups, group)
 
 
 
